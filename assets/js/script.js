3a47661ca23395e1eb96d096523548f4ffd708f4
Ebi <ebiverbeek@gmail.com>
2022-12-06 11:16:38 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var voornaam = "Ebi"; 
+var achternaam = "Verbeek";
+var volledigeNaam = voornaam + achternaam;
+var geboortedag = "21";
+var geboortemaand = "-07"; 
+var geboortejaar = "-2004"; 
+var huidig_jaartal = "2022"
+var leeftijd = 18;
+var dag_van_het_jaar = 340; 
+var straat = "Bloemenlaan"; 
+var huisnummer = "222";
+var woonplaats = "Rozensingel"; 
+var woonachtig_in_utrecht = "False"; 
+var aantal_studenten_in_deze_klas = 21;
+var kosten_van_frikandelbroodje = 1.76;
+var totale_lunchkosten = 73.92;
+
+document.write("Voornaam: " + voornaam);
+document.write("<br>");
+document.write("Achternaam: " + achternaam);
+document.write("<br>");
+document.write("Volledige naam: " + volledigeNaam);
+document.write("<br>");
+document.write("Mijn geboortedatum is: " +geboortedag + geboortemaand + geboortejaar);
+document.write("<br>");
+document.write("Leeftijd: " +leeftijd);
+document.write("<br>");
+document.write("Dag van het jaar: " + dag_van_het_jaar)
+document.write("<br>");
+document.write("Straatnaam: " +straat)
+document.write("<br>");
+document.write("Huisnummer: " + huisnummer)
+document.write("<br>");
+document.write("Woonplaats: " +woonplaats)
+document.write("<br>");
+document.write("Woonachtige in Utrecht: " +woonachtig_in_utrecht)
+document.write("<br>");
+document.write("Aantal studenten: " +aantal_studenten_in_deze_klas)
+document.write("<br>");
+document.write("Prijs frikandelbroodje: " +kosten_van_frikandelbroodje)
+document.write("<br>");
+document.write("Totale kosten 2 frikandelbroodjes per persoon: " +totale_lunchkosten)
+
